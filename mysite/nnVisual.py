7a124c345d31e3dd60766eddae9f62b944c40638
LuiCB <luyima1993@gmail.com>
2018-01-09 10:19:22 -0500

    update nnVisual.py

--- mysite/nnVisual.py
+++ mysite/nnVisual.py
@@ -8,6 +8,8 @@ def runServer(port):
 	subprocess.run(['python3', 'manage.py', 'runserver', port])
 
 if __name__ == "__main__":
-	port = sys.argv[1]
-	print(port) 
+	if (len(sys.argv) != 2):
+		port = '8080'
+	else:
+		port = sys.argv[1] 
 	runServer(port)
